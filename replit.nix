b4c9b8b2d3f64b4d74c30a7d514fce9f9ccb1371
zuliabegum0 <39543008-zuliabegum0@users.noreply.replit.com>
2025-04-27 05:56:38 +0000

    Restored to '72297f81e1079e6d21f07cc0e12d5d145416a2d3'
    Replit-Restored-To: 72297f81e1079e6d21f07cc0e12d5d145416a2d3

--- replit.nix
+++ replit.nix
@@ -1,6 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.zip
     pkgs.ffmpeg
     pkgs.postgresql
   ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 export default defineConfig({
-  base: './',   // super important for ESP
-  plugins: [react()],
+  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
@@ -17,12 +17,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist-built"), // better separate folder
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,  // bundle all JS into one file
-      }
-    }
   },
 });
